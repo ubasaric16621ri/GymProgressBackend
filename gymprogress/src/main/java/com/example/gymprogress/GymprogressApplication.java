8de5ec00274d6ede2e75dcfdf24c60b8b7e92c8e
ubasaric16621ri <ubasaric16621ri@raf.rs>
2024-09-29 01:30:23 +0200

    Initial backend commit

---
+++ gymprogress/src/main/java/com/example/gymprogress/GymprogressApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymprogress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymprogressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymprogressApplication.class, args);
+	}
+
+}
---
+++ gymprogress/src/test/java/com/example/gymprogress/GymprogressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gymprogress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymprogressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
